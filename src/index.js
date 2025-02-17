d9b9fd3cfe6a1efa688eade293ed70aeea2c2291
Angèle van Groesen <a.vangroesen@outlook.com>
2025-02-17 21:40:21 +0100

    Solved Lab

--- src/index.js
+++ src/index.js
@@ -6,27 +6,36 @@ let timer = null; // Variable to store the interval
 
 // ITERATION 1: Add event listener to the start button
 
-// Your code goes here ...
-
-
+let startButton = document.getElementById('start-btn'); //accessing the start button by its id
+startButton.onclick = function() {
+  startCountDown();
+};
 
 
 // ITERATION 2: Start Countdown
-function startCountdown() {
-  console.log("startCountdown called!");
-
-
-  // Your code goes here ...
+function startCountDown() {
+timer = setInterval(function () {
+  if (remainingTime > 0) { // if remaining time is more than 0, show the remaining time
+    console.log(remainingTime);
+    remainingTime--; // time decreasing each second
+  } else { 
+    clearInterval(timer); //stop counting down at 0
+    console.log("startCountDown called!");
+  }
+}, 1000);
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
-
-  // Your code goes here ...
+    const toast = document.getElementById('toast'); // Get the toast card element
+    const toastMessage = document.getElementById('toast-message');
+    toastMessage.innerText = message;
+    toast.classList.add('show');
+    setTimeout(function() {
+      toast.classList.remove('show');
+    }, 3000);
+  }
 
 
 
@@ -35,4 +44,4 @@ function showToast(message) {
 
   // Your code goes here ...
 
-}
+
