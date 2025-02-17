aaa58021a93e8116604ddfc488b8263518b0fa83
Angèle van Groesen <a.vangroesen@outlook.com>
2025-02-17 22:03:19 +0100

    Solved lab repair

--- src/index.js
+++ src/index.js
@@ -6,23 +6,37 @@ let timer = null; // Variable to store the interval
 
 // ITERATION 1: Add event listener to the start button
 
-let startButton = document.getElementById('start-btn'); //accessing the start button by its id
-startButton.onclick = function() {
-  startCountDown();
-};
-
+const startButton = document.querySelector("#start-btn"); //accessing the start button by its id
+startButton.addEventListener("click", startCountDown);
 
 // ITERATION 2: Start Countdown
 function startCountDown() {
-timer = setInterval(function () {
-  if (remainingTime > 0) { // if remaining time is more than 0, show the remaining time
-    console.log(remainingTime);
-    remainingTime--; // time decreasing each second
-  } else { 
-    clearInterval(timer); //stop counting down at 0
-    console.log("startCountDown called!");
-  }
-}, 1000);
+  const timeElement = document.querySelector("#time");
+  const button = document.querySelector("#start-btn");
+  button.disabled = true;
+
+  timer = setInterval(() => {
+    timeElement.textContent = remainingTime;
+
+    if (remainingTime === 0) {
+      clearInterval(timer);
+      showToast(" Lift off! 🚀");
+    }
+
+    // BONUS: ITERATION 5: More Toasts
+    if (remainingTime === 10) {
+      showToast("⏰ Final countdown! ⏰");
+    }
+
+    if (remainingTime === 5) {
+      showToast("Start the engines! 💥");
+    }
+  
+    // IMPORTANT: Remember to decrement the remaining time on each interval
+    remainingTime--;
+    
+  }, 1000);
+
 }
 
 // ITERATION 3: Show Toast
@@ -39,9 +53,5 @@ function showToast(message) {
 
 
 
-
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
 
 
